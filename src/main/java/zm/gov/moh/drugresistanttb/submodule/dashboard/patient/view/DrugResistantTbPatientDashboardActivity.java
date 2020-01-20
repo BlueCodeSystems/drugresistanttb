4383af42657d847e969f76642e03e015464fdf28
Kaunda Kabwe <kay44square@gmail.com>
2020-01-20 18:04:11 +0200

    latest mdr with notification card form loading

--- src/main/java/zm/gov/moh/drugresistanttb/submodule/dashboard/patient/view/DrugResistantTbPatientDashboardActivity.java
+++ src/main/java/zm/gov/moh/drugresistanttb/submodule/dashboard/patient/view/DrugResistantTbPatientDashboardActivity.java
@@ -3,6 +3,7 @@ package zm.gov.moh.drugresistanttb.submodule.dashboard.patient.view;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,18 +14,35 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
+import androidx.viewpager.widget.ViewPager;
+import zm.gov.moh.common.model.VisitMetadata;
 import org.threeten.bp.format.DateTimeFormatter;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import zm.gov.moh.common.base.BaseActivity;
+import zm.gov.moh.common.submodule.dashboard.client.adapter.ClientDashboardFragmentPagerAdapter;
 import zm.gov.moh.core.model.Key;
+import zm.gov.moh.core.model.submodule.BasicModule;
 import zm.gov.moh.core.model.submodule.Module;
 import zm.gov.moh.core.repository.database.Database;
+import zm.gov.moh.core.repository.database.entity.derived.Client;
 import zm.gov.moh.core.utils.BaseApplication;
+import zm.gov.moh.core.utils.Utils;
 import zm.gov.moh.drugresistanttb.R;
 import zm.gov.moh.drugresistanttb.databinding.ActivityDrugResistantTbPatientDashboardBinding;
+import zm.gov.moh.drugresistanttb.submodule.dashboard.patient.adapter.MdrDashboardFragmentPagerAdapter;
+import zm.gov.moh.drugresistanttb.submodule.dashboard.patient.adapter.MdrFormListAdapter;
+import zm.gov.moh.drugresistanttb.submodule.dashboard.patient.model.FormGroup;
 import zm.gov.moh.drugresistanttb.submodule.dashboard.patient.viewmodel.DrugResistantTbPatientDashboardViewModel;
+import zm.gov.moh.drugresistanttb.view.MyDrugResistantTbDialogFragment;
 
 public class DrugResistantTbPatientDashboardActivity extends BaseActivity
         implements BottomNavigationView.OnNavigationItemSelectedListener {
@@ -35,9 +53,11 @@ public class DrugResistantTbPatientDashboardActivity extends BaseActivity
     Bundle mBundle;
     Module vitals;
     long clientId;
+    Client client;
     private BottomNavigationView bottomNavigationView;
     private Fragment fragment;
     private FragmentManager fragmentManager;
+    List<FormGroup> mdrFormLists = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +74,25 @@ public class DrugResistantTbPatientDashboardActivity extends BaseActivity
 
         vitals = ((BaseApplication) this.getApplication()).getModule(BaseApplication.CoreModule.VITALS);
         Database database = viewModel.getRepository().getDatabase();
+
+        ActivityDrugResistantTbPatientDashboardBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_drug_resistant_tb_patient_dashboard);
+        binding.setTitle("MDR Patient Dashboard");
+
+        FormGroup formGroup = new FormGroup();
+        List<BasicModule> list = new ArrayList<>();
+        BasicModule formModule = new BasicModule("Notification Card", NotificationCardFormActivity.class);
+        list.add(formModule);
+
+        formGroup.setTitle("MDR Forms");
+        formGroup.setFormList(list);
+        mdrFormLists.add(formGroup);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        MdrFormListAdapter adapter = new MdrFormListAdapter(this, mdrFormLists, mBundle);
+        ExpandableListView formListView = findViewById(R.id.mdr_adapter_list);
+        formListView.setAdapter(adapter);
+
+
         getViewModel().getRepository().getDatabase().genericDao().getMdrPatientById(clientId)
             .observe(this, patient -> {
                 if (patient == null) {
@@ -62,21 +101,18 @@ public class DrugResistantTbPatientDashboardActivity extends BaseActivity
                 }
         });
 
-        ActivityDrugResistantTbPatientDashboardBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_drug_resistant_tb_patient_dashboard);
-        binding.setTitle("MDR Patient Dashboard");
-
         initToolBar(binding.getRoot());
-        viewModel.getRepository().getDatabase().genericDao().getPatientById(clientId).
+        /*viewModel.getRepository().getDatabase().genericDao().getMdrPatientById(clientId).
                 observe(this, binding::setClient);
         viewModel.getRepository().getDatabase().personAddressDao().findByPersonIdObservable(clientId).
                 observe(this, binding::setClientAddress);
         viewModel.getRepository().getDatabase().locationDao().getByPatientId(clientId).
-                observe(this, binding::setFacility);
+                observe(this, binding::setFacility);*/
 
         // adding patient information in databundle
         viewModel.getRepository().getDatabase().clientDao().findById(clientId)
                 .observe(this, client1 -> {
-                    binding.setClient(client1);
+                    //binding.setClient(client1);
 
                     mBundle.putString(Key.PERSON_FAMILY_NAME, client1.getFamilyName());
                     mBundle.putString(Key.PERSON_GIVEN_NAME, client1.getGivenName());
@@ -94,9 +130,9 @@ public class DrugResistantTbPatientDashboardActivity extends BaseActivity
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.recents_menu_item_id);
 
-        database.genericDao().getPatientById(clientId).observe(this, binding::setClient);
+        database.genericDao().getMdrPatientById(clientId).observe(this, binding::setClient);
         database.personAddressDao().findByPersonIdObservable(clientId).observe(this, binding::setClientAddress);
-        database.locationDao().getByPatientId(clientId, 4L).observe(this, binding::setFacility);
+        database.locationDao().getByPatientId(clientId, 7L).observe(this, binding::setFacility);
         //database.visitDao().getByPatientIdVisitTypeId(clientId, 2L, 3L, 4L, 5L, 6L, 7L).observe(this, viewModel::onVisitsRetrieved);
 
         //set navigation drawer
@@ -106,6 +142,56 @@ public class DrugResistantTbPatientDashboardActivity extends BaseActivity
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return true;
+    }
+
+
+    public Module getVitals() {
+        return vitals;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public DrugResistantTbPatientDashboardViewModel getViewModel() {
+        return viewModel;
+    }
+
+
+    public void startVisit() {
+
+        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        MyDrugResistantTbDialogFragment dialogFragment = new MyDrugResistantTbDialogFragment();
+
+        dialogFragment.show(fragmentManager, "Test");
+    }
+
+    public void selectVisit(int visit) throws IOException {
+
+        switch (visit) {
+
+            case 1:
+
+                VisitMetadata visitMetadata = null;
+                try {
+                    visitMetadata = new VisitMetadata(this, Utils.getStringFromInputStream(this.getAssets().open("visits/str_reg.json")));
+                    mBundle.putSerializable(Key.VISIT_METADATA, visitMetadata);
+                    mBundle.putSerializable(Key.VISIT_STATE, zm.gov.moh.core.model.VisitState.NEW);
+                    this.startModule(BaseApplication.CoreModule.VISIT, mBundle);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case 2:
+
+
+                visitMetadata = new VisitMetadata(this, Utils.getStringFromInputStream(this.getAssets().open("visits/leep.json")));
+                mBundle.putSerializable(Key.VISIT_METADATA, visitMetadata);
+                mBundle.putSerializable(Key.VISIT_STATE, zm.gov.moh.core.model.VisitState.NEW);
+                this.startModule(BaseApplication.CoreModule.VISIT, mBundle);
+
+        }
     }
 }
 
