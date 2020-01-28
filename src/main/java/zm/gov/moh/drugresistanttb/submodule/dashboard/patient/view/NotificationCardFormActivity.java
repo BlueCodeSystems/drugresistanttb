ef33e560d116da2c77be9293cc60229f667d0ba2
Kaunda Kabwe - Work PC <kkabwe@bluecodeltd.com>
2020-01-28 12:40:44 +0200

    all forms showing now. For real...

--- src/main/java/zm/gov/moh/drugresistanttb/submodule/dashboard/patient/view/NotificationCardFormActivity.java
+++ src/main/java/zm/gov/moh/drugresistanttb/submodule/dashboard/patient/view/NotificationCardFormActivity.java
@@ -1,15 +1,34 @@
 package zm.gov.moh.drugresistanttb.submodule.dashboard.patient.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-import zm.gov.moh.drugresistanttb.R;
+import zm.gov.moh.common.base.BaseActivity;
+import zm.gov.moh.common.model.VisitMetadata;
+
+import zm.gov.moh.core.model.Key;
+import zm.gov.moh.core.utils.BaseApplication;
+import zm.gov.moh.core.utils.Utils;
 
 import android.os.Bundle;
 
-public class NotificationCardFormActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class NotificationCardFormActivity extends BaseActivity {
+
+    Bundle mbundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification_card_form);
+        mbundle = getIntent().getExtras();
+
+        VisitMetadata visitMetadata = null;
+        try {
+            visitMetadata = new VisitMetadata(this, Utils.getStringFromInputStream(this.getAssets().open("visits/mdr.json")));
+            mbundle.putSerializable(Key.VISIT_METADATA, visitMetadata);
+            mbundle.putSerializable(Key.VISIT_STATE, zm.gov.moh.core.model.VisitState.NEW);
+            this.startModule(BaseApplication.CoreModule.VISIT, mbundle);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
